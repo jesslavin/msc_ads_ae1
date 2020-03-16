b7a105a042dbf4404f8bf117abedfc0f92127c89
Jessica Lavin <2495543l@student.gla.ac.uk>
2020-03-16 14:38:17 +0000

    algorithms added

--- Part 2/AssessmentSLL.java
+++ Part 2/AssessmentSLL.java
@@ -65,16 +65,24 @@ public class AssessmentSLL<E extends Comparable<E>> {
 	 * add new node to end of list
 	 */
 	public void insertTail(E elem) {
+		// 1. Initialise new node n, set n.next to null
 		Node<E> n = new Node<>(elem, null);
+		// 2. If the SLL is empty:
 		if (first == null) {
+			// a. Set first to n
 			first = n;
+			// 3. Else, if elements are present in the SLL:
 		} else {
+			// a. Initialise new node current, set current to first
 			Node<E> curr = first;
+			// b. Traverse current till it is the last node in the SLL
 			while (curr.next != null) {
 				curr = curr.next;
 			}
+			// c. Set current.next to n
 			curr.next = n;
 		}
+		// 4. Return
 		return;
 	}
 
@@ -85,57 +93,84 @@ public class AssessmentSLL<E extends Comparable<E>> {
 	 *  the method would delete nodes containing badger and dog
 	 */
 	public void deleteAlternate() {
+		// 1. If the SLL is empty, return
 		if (first == null) {
 			return;
 		}
+		// 2. Else, initialise new nodes previous and current, set previous to first and current to first.next
 		Node<E> prev = first;
 		Node<E> curr = first.next;
+		// 3. While neither the previous or current are empty, repeat:
 		while (prev != null && curr != null)
 		{
+			// a. Free current by setting previous.next to current.next
 			prev.next = curr.next;
+			// b. Empty the contents of current
 			curr = null;
+			// c. Set previous to previous.next
 			prev = prev.next;
+			// d. If previous is not empty set current to previous.next
 			if (prev != null) {
 				curr = prev.next;
 			}
 		}
+		// 4. Return
 		return;
 	}
 
+	// 4. Helper function (mergeTwo):
 	public AssessmentSLL<E> mergeTwo(AssessmentSLL<E> listB) {
+		// a. Create new empty SLL, temp
 		AssessmentSLL<E> tempList = new AssessmentSLL<E>();
+		// b. Initialise first node of list a and list b
 		Node<E> listANode = this.first;
 		Node<E> listBNode = listB.first;
+		// c. While a and b are not empty, repeat:
 		while (listANode != null && listBNode != null) {
+			// i. If a < b add a to end of temp, set a to a.next
 			if (listANode.element.compareTo(listBNode.element) < 0) {
 				tempList.insertTail(listANode.element);
 				listANode = listANode.next;
+				// ii. Else add b to end of temp, set b to b.next
 			} else {
 				tempList.insertTail(listBNode.element);
 				listBNode = listBNode.next;
 			}
 		}
+		// d. While a is not empty, repeat:
 		while (listANode != null) {
+			// i. Add a to end of temp, set a to a.next
 			tempList.insertTail(listANode.element);
 			listANode = listANode.next;
 		}
+		// e. While b is not empty, repeat:
 		while (listBNode != null) {
+			// i. Add b to end of temp, set a to b.next
 			tempList.insertTail(listBNode.element);
 			listBNode = listBNode.next;
 		}
+		// f. Return temp
 		return tempList;
 	}
 
+	// Helper function (removeDups):
 	public AssessmentSLL<E> removeDups(){
+		// a. Initialise new node current as list.first
 		Node<E> curr = this.first;
+		// b. While current is not empty:
 		while (curr != null) {
+			// i. Initialise new node counter as current
 			Node<E> counter = curr;
+			// ii.	While counter is not empty and counter equals current, set counter to counter.next
 			while(counter != null && counter.element == curr.element){
 				counter = counter.next;
 			}
+			// c. Set current.next to counter
 			curr.next = counter;
+			// d. Set current to current.next
 			curr = curr.next;
 		}
+		// e. Return
 		return this;
 	}
 
@@ -146,10 +181,15 @@ public class AssessmentSLL<E extends Comparable<E>> {
 	 * with the new name of this class
 	 */
 	public AssessmentSLL<E> merge(AssessmentSLL<E> list1, AssessmentSLL<E> list2) {
+		// 1. Create new empty SLL, L3
 		AssessmentSLL<E> mergedList = new AssessmentSLL<E>();
+		// 2. Set L3 to result of helper function mergeTwo (L, L1)
 		mergedList = this.mergeTwo(list1);
+		// 3. Set L3 to result of helper function mergeTwo (L2, L3)
 		mergedList = list2.mergeTwo(mergedList);
+		// 5. Remove any duplicates from L3 using helper function (removeDups):
 		mergedList = mergedList.removeDups();
+		// 6. Return L3
 		return mergedList;
 	}
 }
